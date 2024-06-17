ac7713611c661cb3818850937e9ed20db870acbe
Jiyuan Zhao <jy.zhao@outlook.com>
2024-06-17 16:01:54 -0700

    update project section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import cspo from "../../Assets/Projects/cspo.png";
+import dcat from "../../Assets/Projects/dcat.png";
+import ccms from "../../Assets/Projects/ccms.png";
+import creatorcon from "../../Assets/Projects/creatorcon.png";
+import convergesecurity from "../../Assets/Projects/convergesecurity.png";
+import smartauction from "../../Assets/Projects/smartauction.png";
+import personafi from "../../Assets/Projects/personafi.png";
+import collegeval from "../../Assets/Projects/collegeval.png";
 
 function Projects() {
   return (
@@ -15,7 +17,7 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          My <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
           Here are a few projects I've worked on recently.
@@ -23,68 +25,89 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={cspo}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="Cloud Security Policy Orchestration"
+              description="Cloud Security Policy Orchestration (CSPO) (FKA CloudQuest Guardian, acquired in 2021) is a policy-driven approach to shift-left security, can help with broad visibility into cloud assets across the multi-cloud landscape and enforce security policy-as-code uniformly across the software development life cycle."
+              ghLink=""
+              demoLink="https://www2.deloitte.com/us/en/pages/risk/solutions/cloud-security-policy-orchestration.html/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={ccms}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Cyber Cloud Managed Services"
+              description="Cyber Cloud Managed Services can help provide around-the-clock security protection and monitoring plus a wide range of options to help you move forward with confidence."
+              ghLink=""
+              demoLink="https://www2.deloitte.com/us/en/pages/risk/solutions/cloud-security-management-services.html"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={convergesecurity}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="ConvergeSECURITY"
+              description="ConvergeSECURITY empowers enterprises to accelerate cloud transformation efforts with a combination of artificial intelligence (AI)-enabled cloud security and compliance product solutions, consulting expertise, and tailored resources leveraging actionable security threat intelligence and Amazon Security Lake (ASL), all in conjunction with the AWS Global Partner Security Initiative."
+              ghLink=""
+              demoLink="https://www2.deloitte.com/us/en/pages/risk/solutions/ConvergeSECURITY-cloud-security.html"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={leaf}
+              imgPath={dcat}
               isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              title="Capital Allocation and Portfolio Optimization"
+              description="Capital Allocation and Portfolio Optimization service is designed to address these top pain points identified by asset-intensive clients in their investment decision making processes."
+              ghLink=""
+              demoLink="https://www.deloitte.com/uk/en/services/consulting/services/capital-allocation-and-portfolio-optimisation.html"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={suicide}
+              imgPath={smartauction}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="SmartAuction by Ally"
+              description="Built for Ally, SmartAuction is an industry-leading online auto auction that offers access to a virtual inventory of wholesale vehicles for eligible dealers of all brands. It offers a virtual inventory of wholesale vehicles that select, approved dealers have the opportunity to buy before they are."
+              ghLink=""
+              demoLink="https://www.smartauctionlogin.com/home/#"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={collegeval}
+              isBlog={false}
+              title="CollegeVal"
+              description="CollegeVal empowers you to make smarter decisions about your college education.  Unveil the true cost of your college options and calculate your potential return on investment (ROI) after graduation."
+              // ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={emotion}
+              imgPath={personafi}
               isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              title="PersonaFi"
+              description="PersonaFi is your friendly AI assistant, here to guide you towards financial success! Set goals, track expenses, and create a personalized budget that fits your lifestyle; Explore different financial scenarios and see how your choices impact your long-term goals; Automate your savings, identify hidden costs, and make informed decisions about your financial future."
+              // ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={creatorcon}
+              isBlog={false}
+              title="CreatorCon"
+              description="CreatorConnect enables creators to monetize their content through brand partnerships, sponsorships, and advertising deals while providing businesses with a seamless way to promote their products and services through authentic and engaging content. Additionally, it offers advanced content delivery and data streaming capabilities to enhance the user experience and provide real-time insights."
+              // ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
             />
           </Col>
         </Row>
